6b5f687d407d6b1b271c3eebcbce2eca171f850e
AndyLu <810376@gmail.com>
2024-10-10 22:10:45 +0800

    刪除預設版面view，加入模板NavBar和footer

--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
 import './assets/main.css'
+import './assets/style.css'
+
+// 引入 Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css'
+// 引入 Bootstrap JavaScript
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+import '@fortawesome/fontawesome-free/css/all.css';
+
+import 'jquery/dist/jquery.min.js';
+
+import WOW from 'wowjs';
+new WOW.WOW().init();
+
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
