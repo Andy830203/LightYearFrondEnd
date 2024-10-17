ce2a9434b165be5372ad7c2e8b94cf671a4edab5
Ted Sun <catchsun5305@gmail.com>
2024-10-17 13:50:38 +0800

    Revert "修正路徑import"
    This reverts commit 92d361c2d054320c8fce80f1727f6535995caecb.

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
-import Hb1 from '@/views/hb1.vue'
-import { state } from '@/global_value.js';  // 從 state.js 
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
