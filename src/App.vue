ab6b504504fa28cf512886426e7212ff0edd175d
Ted Sun <catchsun5305@gmail.com>
2024-10-17 13:51:24 +0800

    Revert "1.src->"
    This reverts commit 2b02b877271dbc32f7ed4155d86024c1eeaffa9b.

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,6 @@
 import { RouterLink, RouterView } from 'vue-router'
 import NavBar from './components/NavBar.vue';
 import FooterBar from './components/FooterBar.vue';
-import { provide } from 'vue';
-import { state } from '@/global_value'; // 從外部文件引入狀態
-provide('isDisabled', state);// 提供狀態給子組件
 </script>
 
 <template>
