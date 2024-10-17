68db57eea32998dc7404aece7383ac67cb2602bf
Ted Sun <catchsun5305@gmail.com>
2024-10-17 13:51:36 +0800

    Revert "router->index:"
    This reverts commit b5ee6c9a9435a52e6a532a93e0033bb10228bbc0.

--- src/router/index.js
+++ src/router/index.js
@@ -10,24 +10,8 @@ const router = createRouter({
       component: HomeView,
       name: 'home',
       alias: '/index'    //http://localhost:5173/index
-    },
-    {
-      //http://localhost:5173/location
-      path: '/location',
-      component: Hb1,
-      beforeEnter: (to, from) => {//當進入此組件時觸發
-        state.isDisabled = true;
-        console.log("進入"+state.isDisabled);
-      },
-    },
+    }
   ]
 })
-// 使用全局afterEach守衛，路由切換後恢復組件的狀態
-router.afterEach((to, from) => {
-  if (to.path !== '/location') {
-    // 當離開 /location 路由時恢復 A 組件的顯示
-    state.isDisabled = false;
-    console.log("離開後"+state.isDisabled);
-  }
-});
+
 export default router
