b71c74dd48b56ee8c9a89f6556c860c74ca4da61
Ted Sun <catchsun5305@gmail.com>
2024-10-19 03:46:10 +0800

    新增標頭

--- src/components/event/SelectFieldComponent.vue
+++ src/components/event/SelectFieldComponent.vue
@@ -2,7 +2,6 @@
 import { ref } from 'vue';
 
 const props = defineProps({
-    Type: String,
     Id: String,
     Label: String,
     LabelCol: Number,
@@ -10,20 +9,18 @@ const props = defineProps({
     SpaceCol: Number
 });
 
-const model = defineModel();
+const items = defineModel();
 </script>
 
 <template>
     <div>
         <div class="col-3 offset-1">
-            <label for="eveType" class="col-form-label">活動類別</label>
+            <label :for="Id" class="col-form-label">{{ Label }}</label>
         </div>
         <div class="col-5">
             <!-- <input type="number" class="form-control" id="eveType"> -->
-            <select class="form-select" name="" id="eveType">
-                <option value="">type 1</option>
-                <option value="">type 2</option>
-                <option value="">type 3</option>
+            <select class="form-select" name="" :id="Id">
+                <option v-for="item in items" :value="item">{{ item }}</option>
             </select>
         </div>
         <div class="col-3"></div>
--- src/views/event/SignUpView.vue
+++ src/views/event/SignUpView.vue
@@ -1,13 +1,44 @@
 <script setup>
+import { ref } from 'vue';
+
+
+let eData = {
+    'id': 0,
+    'eventName': 'title',
+    'fee': '300',
+    'eventPeriods': [
+        { 'name': '第一梯次', 'time': '20240801-00:00:00' },
+        { 'name': '第二梯次', 'time': '20240802-00:00:00' },
+        { 'name': '第三梯次', 'time': '20240803-00:00:00' },
+    ]
+};
+
+const eventData = ref(eData);
 
 </script>
 
 <template>
     <div class="mt-3 container">
         <form>
+            <div class="d-flex justify-content-around w-25">
+                <h2>{{ eventData.eventName }}</h2>
+                <select :name="eventData.eventName" :id="eventData.id">
+                    <option v-for="period in eventData.eventPeriods" :value="period.time">
+                        {{ period.name }}
+                    </option>
+                </select>
+            </div>
+            <div class="col-6 offset-3">
+                <div class="box"></div>
+            </div>
 
         </form>
     </div>
 </template>
 
-<style lang="css" scoped></style>
+<style lang="css" scoped>
+.box {
+    border: 1px solid salmon;
+    height: 100px;
+}
+</style>
