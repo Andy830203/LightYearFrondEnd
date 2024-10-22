ed5c405e637b8d8bd6ce4c244945240fa5b85e72
AndyLu <810376@gmail.com>
2024-10-22 14:13:58 +0800

    chatGPT整理了NavBar.vue的template

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -7,67 +7,64 @@ m_e_onBeforeUnmount()
 </script>
 
 <template>
-<!-- 原有的 -->
-    <!-- <nav class="navbar navbar-expand-lg fixed-top navbar-light bg-light shadow"> -->
-    <!-- 更改的 -->
-    <nav class="navbar navbar-expand-lg fixed-top navbar-light bg-light shadow" id="navbar_e1" @mouseenter="m_e_move"  @mouseleave="m_e_leave" :class="navClass">
+    <!-- 主導航列 -->
+    <nav class="navbar navbar-expand-lg fixed-top navbar-light bg-light shadow" 
+        id="navbar_e1" 
+        @mouseenter="m_e_move"  
+        @mouseleave="m_e_leave" 
+        :class="navClass">
+        
         <div class="container-fluid">
-            <!-- <a class="navbar-brand" href="/">食益光年</a> -->
-            <!-- logo圖路徑之後用ref路徑指向一個變數，下面佔位子的導覽列也是 -->
-            <a class="navbar-brand" href="/"><img src="../assets/images/members/logo圖0.png"></a>
+            <!-- Logo -->
+            <a class="navbar-brand" href="/">
+                <img src="../assets/images/members/logo圖0.png" alt="Logo">
+            </a>
+            
+            <!-- 漢堡選單按鈕（小螢幕時） -->
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
+
+            <!-- 導覽列內容 -->
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <!-- 地圖連結 -->
                     <li class="nav-item">
                         <router-link to="/location" class="nav-link">地圖</router-link>
                     </li>
+
+                    <!-- 下拉選單 -->
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
                             data-bs-toggle="dropdown" aria-expanded="false">
                             下拉選單預留
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="#">下拉1</a>
-                            </li>
-                            <li><a class="dropdown-item" href="">下拉2</a>
-                            </li>
+                            <li><a class="dropdown-item" href="#">下拉1</a></li>
+                            <li><a class="dropdown-item" href="#">下拉2</a></li>
                         </ul>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="./faq.html">常見問題</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="./form.html">聯絡我們</a>
-                    </li>
-                    <!--會員-->
+
+                    <!-- 其他連結 -->
+                    <li class="nav-item"><a class="nav-link" href="./faq.html">常見問題</a></li>
+                    <li class="nav-item"><a class="nav-link" href="./form.html">聯絡我們</a></li>
+
+                    <!-- 會員專區下拉選單 -->
                     <li class="nav-item dropdown">
-                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
                             data-bs-toggle="dropdown" aria-expanded="false">
-                        <i style="color: black;" class="bi bi-person"></i> 會員專區
-                         </a>
-                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <li>
-                          <a class="dropdown-item" href="/MemberInFo">
-                         <i class="bi bi-info-circle"></i> 會員資訊
-                         </a>
+                            <i class="bi bi-person" style="color: black;"></i> 會員專區
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="/MemberInFo"><i class="bi bi-info-circle"></i> 會員資訊</a></li>
+                            <li><a class="dropdown-item" href="/MemberSettingView"><i class="fa-solid fa-gear"></i> 設定</a></li>
+                            <li><a class="dropdown-item" href="/MemberFavorite"><i class="fa-solid fa-heart"></i> 我的收藏</a></li>
+                        </ul>
                     </li>
-                    <li>
-                          <a class="dropdown-item" href="/MemberSettingView">
-                            <i class="fa-solid fa-gear"></i> 設定
-                         </a>
-                    </li>
-                    <li>
-                          <a class="dropdown-item" href="/MemberFavorite">
-                            <i class="fa-solid fa-heart"></i> 我的收藏
-                         </a>
-                    </li>
-                     </ul>                    
-                    </li>
-                   
+
+                    <!-- 活動專區 -->
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
                             data-bs-toggle="dropdown" aria-expanded="false">
@@ -80,36 +77,28 @@ m_e_onBeforeUnmount()
                         </ul>
                     </li>
                 </ul>
-            
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
-            </ul>
-            <div class="d-flex">
-                <!--會員-->               
-                <!-- <a href="javascript:;" data-bs-toggle="modal" data-bs-target="#exampleModal"
-                    class="me-1 text-black"> -->
+
+                <!-- 會員與購物車按鈕 -->
+                <div class="d-flex">
                     <RouterLink to="/login" class="me-1 text-black">
                         <i class="bi bi-person-fill fs-3 text-primary"></i>
                     </RouterLink>
-                <!-- </a> -->
-                <a data-bs-toggle="offcanvas" href="#offcanvasExample" role="button"
-                    aria-controls="offcanvasExample" class="me-1 text-black"><i
-                        class="bi bi-cart-fill fs-3 text-primary"></i></a>
-            </div>
-        </div>
+                    <a data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample" class="me-1 text-black">
+                        <i class="bi bi-cart-fill fs-3 text-primary"></i>
+                    </a>
+                </div>
             </div>
         </div>
     </nav>
+
+    <!-- 備用導航列 -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light" v-if="!state.isDisabled">
         <div class="container-fluid">
-            <a class="navbar-brand" href="/"><img src="../assets/images/members/logo圖0.png"></a>
+            <a class="navbar-brand" href="/"><img src="../assets/images/members/logo圖0.png" alt="Logo"></a>
             <div class="navbar-brand">.</div>
         </div>
     </nav>
-    <!-- header end -->
-
 </template>
-
 
 
 <style lang="css" scoped>
