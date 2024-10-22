9dc7ca516439c672908a90bac2e3b7cf8aa3afc2
AndyLu <810376@gmail.com>
2024-10-22 09:58:19 +0800

    shopindex元件新增

--- src/views/shop/ShopIndexView.vue
+++ src/views/shop/ShopIndexView.vue
@@ -1,6 +1,9 @@
 <script setup>
     import ProductCard from '@/components/shop/ProductCard.vue';
     import SearchBar from '@/components/shop/SearchBar.vue';
+    import SortingSelection from '@/components/shop/SortingSelection.vue';
+    import CategoryFilter from '@/components/shop/CategoryFilter.vue';
+    import PriceRangeBar from '@/components/shop/PriceRangeBar.vue';
 </script>
 
 <template>
@@ -11,17 +14,24 @@
                 <div class="col-lg-12">
                     <div class="row g-4">
                         <div class="col-xl-3">
-                            <SearchBar></SearchBar>
                             <!-- 搜尋 -->
+                            <SearchBar></SearchBar>
                         </div>
                         <div class="col-6"></div>
                         <div class="col-xl-3">
                             <!-- 排序 -->
+                             <SortingSelection></SortingSelection>
                         </div>
                     </div>
                     <div class="row g-4">
                         <div class="col-lg-3">
-                            <!-- 篩選器 -->
+                            <div class="row g-4">
+                                <!-- 類別選取 -->
+                                 <CategoryFilter></CategoryFilter>
+                                <!-- 篩選器 -->
+                                 <PriceRangeBar></PriceRangeBar>
+                            </div>
+                            
                         </div>
                         <div class="col-lg-9">
                             <div class="row g-4 justify-content-center">
