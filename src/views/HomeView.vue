982d437c9384deba1ebbf8ec8f6ed6982c4e6632
steven <mistake123415@gmail.com>
2024-10-24 01:05:11 +0800

    主頁布局

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,11 +3,44 @@
 </script>
 
 <template>
-    <div>
-        <h2>Home Page</h2>
+<div style="text-align: center;">
+    <div id="id_1">
+        <h1>主題介紹</h1>
     </div>
+    <div id="id_2">
+        <h1>輪播</h1>
+    </div>
+    <div id="id_3">
+        <h1>使用說明</h1>
+    </div>
+    <div id="id_4">
+        <h1>統計(直接擴展高度)</h1>
+    </div>
+</div>
 </template>
 
 <style lang="css" scoped>
 
+div{
+    color: black;
+}
+#id_1{
+    background-color: aqua;
+    height: 500px;
+    }
+#id_2{
+    background-color: red;
+    height: 800px;
+    }
+#id_3{
+    background-color: rgb(0, 255, 136);
+    height: 300px;
+    }
+#id_4{
+    background-color: rgb(197, 38, 144);
+    height: 500px;
+    }
+h1{
+top: 50%;
+}
 </style>
