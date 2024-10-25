bc7a295342222cf206e8f80de807c272e27b39c7
AndyLu <810376@gmail.com>
2024-10-25 14:29:08 +0800

    排序component加入與shopindex設定

--- src/views/shop/ShopIndexView.vue
+++ src/views/shop/ShopIndexView.vue
@@ -12,7 +12,7 @@
     const terms = ref({
         "keyword": "",
         "categoryId": 0,
-        "sortBy": "Id",
+        "sortBy": "default",
         "page": 1,
     })
 
@@ -23,15 +23,21 @@
     })
 
     watchEffect(async function() {
-        const response = await fetch(API_URL, {
-            method: 'POST',
-            body: JSON.stringify(terms.value),
-            headers: { 'Content-Type': 'application/json' },
-            mode: 'cors'  // 啟用 CORS 模式
-        })
-        const datas = await response.json()
-        result.value.totalPages = datas.totalPages
-        result.value.products = datas.productsResult
+        try {
+            const response = await fetch(API_URL, {
+                method: 'POST',
+                body: JSON.stringify(terms.value),
+                headers: { 'Content-Type': 'application/json' },
+                mode: 'cors'  // 啟用 CORS 模式
+            })
+            const datas = await response.json()
+            result.value.totalPages = datas.totalPages
+            result.value.products = datas.productsResult
+        }
+        catch (error) {
+            console.error('Error fetching sorted products:', error);
+        }
+        
     })
 
     const pagingHandler = function(page) {
@@ -41,6 +47,13 @@
     const searchHandler = function(keyword) {
         terms.value.keyword = keyword
     }
+
+    // 監聽排序變更
+    const handleSortChange = (newSort) => {
+        console.log(newSort);
+        terms.value.sortBy = newSort; // 更新選擇的排序選項
+        terms.value.page = 1;
+    };
 
     // const CategoryHandler = function() {}
 </script>
@@ -59,7 +72,7 @@
                         <div class="col-6"></div>
                         <div class="col-xl-3">
                             <!-- 排序 -->
-                             <SortingSelection></SortingSelection>
+                             <SortingSelection @sortChanged="handleSortChange"></SortingSelection>
                         </div>
                     </div>
                     <div class="row g-4">
