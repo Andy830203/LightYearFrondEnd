4861da1e8b2e2a6c0a24f697f164bf7a86c24db9
Ted Sun <catchsun5305@gmail.com>
2024-10-30 14:20:18 +0800

    1. start 連到後端

--- src/components/event/PlacesComponent.vue
+++ src/components/event/PlacesComponent.vue
@@ -17,6 +17,8 @@ const demoList = [
 const enableDragable = ref(false)
 const demo = ref(demoList)
 const newListOrder = ref([])
+
+const model = defineModel()
 //props
 
 //drag
@@ -90,43 +92,106 @@ const initDrag = () => {
     }
 }
 </script>
-
+<!-- from: https://devdevout.com/css/css-list-styles -->
 <template>
     <div>
         <input type="checkbox" v-model="enableDragable" @change="initDrag">開啟拖放
-        <!-- <draggableComponent v-if="enableDragable" v-model="demo" tag="ul" class="DragableOn">
-            <li :draggable="enableDragable" v-for="(item, index) in demo" :key="index"> {{ item.order }}-{{ item.locName }}
-                [<span class="nowIndex"></span>]
-            </li>
-        </draggableComponent> -->
-        <ul v-if="enableDragable" class="DragableOn">
-            <li :draggable="enableDragable" v-for="item in demo" :key="item.id" :data-key="item.id"> {{ item.order }}-{{
-                item.locName }}
-                [<span class="nowIndex"></span>] <!-- <span class="hiddenKey">{{ item.id }}</span> -->
-            </li>
-        </ul>
-
-        <ul v-else>
-            <li :draggable="enableDragable" v-for="item in demo" :key="item.id"> {{ item.order }}-{{ item.locName }}
-            </li>
-        </ul>
-
+        <div class="list">
+            <ul v-if="enableDragable" class="DragableOn">
+                <li :draggable="enableDragable" v-for="item in demo" :key="item.id" :data-key="item.id">
+                    <span class="tspan tspan-padding">{{ item.order }}-{{ item.locName }} [<span
+                            class="nowIndex"></span>]</span>
+                </li>
+            </ul>
+            <ul v-else class="DragableOff">
+                <li :draggable="enableDragable" v-for="item in demo" :key="item.id">
+                    <span class="tspan tspan-padding">{{ item.order }}-{{ item.locName }}</span>
+                </li>
+            </ul>
+        </div>
         <button class="btn btn-primary" @click="updateOrder(newListOrder)"> 更改 </button>
     </div>
 </template>
 
 <style lang="css" scoped>
 ul.DragableOn {
-    border: 1px solid salmon;
+    /* border: 1px solid salmon; */
 }
 
 ul.DragableOn li {
     background-color: #000;
     color: #fff;
-    border: 1px solid aliceblue;
+    /* border: 1px solid aliceblue; */
 }
 
-.hiddenKey {
+ul.DragableOff li {
+    background-color: #000;
+}
+
+.hidden {
     display: none;
+}
+
+.list {
+    position: relative;
+}
+
+.list h2 {
+    color: #fff;
+    font-weight: 700;
+    letter-spacing: 1px;
+    margin-bottom: 10px;
+}
+
+.list ul {
+    position: relative;
+}
+
+.list ul li {
+    position: relative;
+    left: 0;
+    color: #fce4ec;
+    list-style: none;
+    margin: 4px 0;
+    border-left: 2px solid #f50057;
+    transition: 0.5s;
+    cursor: pointer;
+}
+
+.list ul li:hover {
+    left: 10px;
+}
+
+.list ul li span.tspan {
+    position: relative;
+    /* padding: 8px;
+    padding-left: 12px; */
+    display: inline-block;
+    z-index: 1;
+    transition: 0.5s;
+}
+
+.list ul li span.tspan-padding {
+    padding: 8px;
+    padding-left: 12px;
+}
+
+.list ul li:hover span.tspan {
+    color: #111;
+}
+
+.list ul li:before {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: #f50057;
+    transform: scaleX(0);
+    transform-origin: left;
+    transition: 0.5s;
+}
+
+.list ul li:hover:before {
+    transform: scaleX(1);
 }
 </style>
