77def1a6bd67903502eb9cb8b17f9e279687ad2c
steven <mistake123415@gmail.com>
2024-11-03 21:58:27 +0800

    部署右側nav(網路找的)

--- src/views/hb1.vue
+++ src/views/hb1.vue
@@ -3,16 +3,28 @@
 import { map_init, mapHeight } from '@/hb1_t_js/GM_c.js';
 //@/hb1_t_js/FT_c.js，footer專用js
 import { FT_set } from '@/hb1_t_js/FT_c.js';
-
+import '@/hb1_t_js/sidebar.css';
 map_init();//地圖初始化
 FT_set()//設置footer是否顯示
 </script>
 
 <template>
     <div id="map"></div>
-
+    <div id="nav-bar">
+        <input id="nav-toggle" type="checkbox"/>
+        <div id="nav-header">
+          <p id="nav-title">C<i class="fab fa-codepen"></i>DEPEN</p>
+          <label for="nav-toggle"><span id="nav-toggle-burger"></span></label>
+          <hr/>
+        </div>
+        <div id="nav-content">
+          <div class="nav-button"><i class="fas fa-palette"></i><span>Your Work</span></div>
+          <div id="nav-content-highlight"></div>
+        </div>
+        <input id="nav-footer-toggle" type="checkbox"/>
+      </div>
 </template>
-
+<!-- <style src="src/hb1_t_js/sidebar.css" scoped></style> -->
 <style>
     #map {
         height: v-bind(mapHeight); /* 綁定動態的 mapHeight */
