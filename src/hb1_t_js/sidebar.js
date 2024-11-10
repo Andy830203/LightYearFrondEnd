6dedaccbe2572fead9d41d5f4f67695b6119e909
steven <mistake123415@gmail.com>
2024-11-11 02:05:38 +0800

    實作點擊

--- src/hb1_t_js/sidebar.js
+++ src/hb1_t_js/sidebar.js
@@ -21,16 +21,21 @@ export function init_sidebar(){
         link.href = 'https://cdn.datatables.net/1.10.24/css/jquery.dataTables.min.css';
         document.head.appendChild(link);
         // 初始化 DataTable
-        $('#side_dt').DataTable({
+        const table = $('#side_dt').DataTable({
+            responsive: true, //響應式
             lengthMenu: [[10, 20, 30, -1], [10, 20, 30, "All"]],
             searching: false,
             pageLength: 8,
             language: {
                 lengthMenu: "前往 _MENU_",
                 info: ""
-            },    
+            },
             scrollX: true, // 啟用橫向滾動
-            responsive: true,
+            
+        });
+        $('#side_dt tbody').on('click', 'td', function() {
+            var cellData = table.cell(this).data(); // 獲取單元格的數據
+            alert('你點擊了: ' + cellData); // 彈出 alert 視窗
         });
     });
 }
--- src/hb1_t_js/sidebar.vue
+++ src/hb1_t_js/sidebar.vue
@@ -7,14 +7,14 @@
   <div class="container d-flex flex-column flex-shrink-0 p-3 text-white bg-dark nav-data"
        style="width: 20vw;position: absolute; right: 2vw; top: 5vw; border-radius: 20px;">
     <div class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-      <span class="fs-4">導覽</span>
+      <span class="fs-4">活動一覽</span>
     </div>
     <hr>
     <table id="side_dt" class="display">
       <thead>
         <tr>
-          <th>Name</th>
-          <th>Position</th>
+          <th>活動名稱</th>
+          <th>位置</th>
         </tr>
       </thead>
       <tbody>
